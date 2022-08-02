826c33e71d0ec3757b28a3087388fb6e6ae7c256
ecamenen <etienne.camenen@gmail.com>
2022-08-02 20:55:14 +0200

    Remove get_golem_config

--- R/app_config.R
+++ R/app_config.R
@@ -5,32 +5,5 @@
 #'
 #' @noRd
 app_sys <- function(...) {
-    system.file(..., package = "tcgaViz")
-}
-
-
-#' Read App Config
-#'
-#' @param value Value to retrieve from the config file.
-#' @param config GOLEM_CONFIG_ACTIVE value. If unset, R_CONFIG_ACTIVE.
-#' If unset, "default".
-#' @param use_parent Logical, scan the parent directory for config file.
-#'
-#' @noRd
-get_golem_config <- function(
-    value,
-    config = Sys.getenv(
-        "GOLEM_CONFIG_ACTIVE",
-        Sys.getenv(
-            "R_CONFIG_ACTIVE",
-            "default"
-        )
-    ),
-    use_parent = TRUE) {
-    config::get(
-        value = value,
-        config = config,
-        file = app_sys("golem-config.yml"),
-        use_parent = use_parent
-    )
+  system.file(..., package = "multiAnalysis")
 }
