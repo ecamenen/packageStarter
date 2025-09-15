036523d5d1f9ce5f7e9019ad4457f5923ca1fb55
Etienne Camenen <etienne.camenen@gmail.com>
2025-09-15 11:44:50 +0200

    Remove docker configuration fileds

--- docker_launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TOOL_NAME=$(Rscript -e "cat(desc::desc_get_field('Package'))")
-TOOL_VERSION=$(Rscript -e "cat(desc::desc_get_field('Version'))")
-DOCKER_NAME="starter"
-DOCKER_VERSION="1.0.0"
-
-docker build -t ${DOCKER_NAME}:${DOCKER_VERSION} --build-arg TOOL_NAME=${TOOL_NAME} --build-arg TOOL_VERSION=${TOOL_VERSION} dev
